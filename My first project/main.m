a114bd3d5204b3f5645ee5050d730e46367a9fe4
Nadia Benavides <Nadia@Nadias-MacBook-Air.local>
2014-08-11 19:46:25 -0700

    Initial Commit

---
+++ My first project/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  My first project
+//
+//  Created by Nadia Benavides on 8/11/14.
+//  Copyright (c) 2014 ___NadiaBenavides___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
